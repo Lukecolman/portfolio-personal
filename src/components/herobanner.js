a786801a54503fd3ec6d04a1694ede02fea1f885
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-04-13 02:00:43 -0300

    Herobanner new design

--- src/components/herobanner.js
+++ src/components/herobanner.js
@@ -7,31 +7,50 @@ export default function HeroBanner({}) {
                     lg:tracking-[-0.8rem]
                     '>
                     <div
-                        className='text-[7.7rem] leading-tight 
-                        lg:text-[480px] lg:leading-none
+                        className='text-[7.7rem] leading-tight md:text-[280px] md:leading-none
+                        lg:text-[480px] 
                         '>
                         ルウク
                     </div>
                     <div
-                        className='text-[7.5rem] leading-[1.3] pb-2
-                        lg:text-[480px] lg:leading-none lg:-mt-12
+                        className='text-[7.5rem] leading-[1.3] pb-2 md:text-[280px] md:leading-none
+                        lg:text-[480px]  lg:-mt-12
                         '>
                         コルマン
                     </div>
                 </div>
 
-                <div className='text-lk-white z-50 absolute lg:pt-6'>
-                    <div
-                        className='font-crimson font-semibold  text-4xl 
-                        lg:text-[4.8rem] 
-                        '>
-                        <span>Hi &nbsp; I’m</span>
+                <div className='text-lk-white z-50 absolute lg:pt-6 flex flex-col-reverse lg:flex-row '>
+
+                    {/* FRONTEND DEVELOPER */}
+                    <div className= ' md:block  leading-none -mt-3 md:text-[24px] lg:mt-0 lg:text-[36px] lg:mr-[4vw] font-semibold justify-center content-center '>
+                        Frontend <br className="hidden lg:block"/>
+                        Developer
                     </div>
-                    <div
-                        className='font-haetten text-[10.6rem] leading-none -mt-8 	
-                        lg:text-[22.5rem] lg:-mt-12
-                        '>
-                        LUKE
+
+                    {/* HI IM LUKE */}
+                    <div>
+
+                        <div
+                            className='font-crimson font-semibold  text-4xl md:text-[3.5rem]
+                            lg:text-[4.8rem] 
+                            '>
+                            <span>Hi &nbsp; I’m</span>
+                        </div>
+
+                        <div
+                            className='font-haetten text-[10.6rem] leading-none -mt-8 md:text-[16.5rem]	md:-mt-11
+                            lg:text-[22.5rem] lg:-mt-12
+                            '>
+                            LUKE
+                        </div>
+
+                    </div>
+
+                    {/* LOCATED IN ARGENTINA */}
+                    <div className='hidden text-[36px] leading-none ml-[4vw] font-semibold justify-center content-center lg:block'>
+                        Located in <br/>
+                        Argentina
                     </div>
                 </div>
             </div>
